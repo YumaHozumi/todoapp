ee9bb8f66152e5077f3e3ba19f8cd81b97c86056
YumaHozumi <cpucmptrpython72@gmail.com>
2022-09-02 01:33:30 +0900

    spring-bootの初期生成ファイル

---
+++ spring-todoapp/src/main/java/dev/itboot/todo/SpringTodoappApplication.java
@@ -0,0 +1,13 @@
+package dev.itboot.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTodoappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTodoappApplication.class, args);
+	}
+
+}
---
+++ spring-todoapp/src/test/java/dev/itboot/todo/SpringTodoappApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.itboot.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTodoappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
