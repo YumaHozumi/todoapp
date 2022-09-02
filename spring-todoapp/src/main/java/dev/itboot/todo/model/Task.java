ceeddda52e604c37c792ce072ddd5a8aa5ded474
YumaHozumi <cpucmptrpython72@gmail.com>
2022-09-03 01:28:36 +0900

    タスクとユーザのモデルを追加

---
+++ spring-todoapp/src/main/java/dev/itboot/todo/model/Task.java
@@ -0,0 +1,34 @@
+package dev.itboot.todo.model;
+
+import java.util.Date;
+
+import javax.validation.constraints.Future;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Todoを表すクラス
+ * @author YumaHozumi
+ *
+ */
+@Setter
+@Getter
+public class Task {
+	private  Long taskId;
+	
+	@NotBlank
+	@Size(max = 254)
+	private String content;
+	
+	@NotBlank
+	@Size(max = 32, min = 10)
+	private String userId;
+	
+	@Future
+	private Date schedule;
+	
+	private int priority;
+}
---
+++ spring-todoapp/src/main/java/dev/itboot/todo/model/User.java
@@ -0,0 +1,28 @@
+package dev.itboot.todo.model;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * ユーザを表すクラス
+ * @author YumaHozumi
+ *
+ */
+@Setter
+@Getter
+public class User {
+	@NotBlank
+	@Size(max = 32, min = 10)
+	private String id;
+	
+	@NotBlank
+	@Size(max = 32, min = 10)
+	private String password;
+	
+	@NotBlank
+	@Size(max = 32)
+	private String nickname;
+}
