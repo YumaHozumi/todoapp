3e266b77d92ffa54c2e42c1a9598643929fedcda
YumaHozumi <cpucmptrpython72@gmail.com>
2022-09-11 18:48:46 +0900

    h2-consoleへのアクセスを拒否するように変更
    (cherry picked from commit 6b188429fe4d821a4986587be4c3dc4f9c237e94)
    (cherry picked from commit 253a92b8a8dec774db60389472559dabd01ef7f3)

--- spring-todoapp/src/main/java/dev/itboot/todo/config/SecurityConfig.java
+++ spring-todoapp/src/main/java/dev/itboot/todo/config/SecurityConfig.java
@@ -35,7 +35,6 @@ public class SecurityConfig {
 				//WebSecurityConfigurerAdapterは現在非推奨
 				.requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll() //cssなどはログインなしでもアクセス可
 				.mvcMatchers("/register", "/login").permitAll()
-				.antMatchers("/h2-console/**").permitAll()
 				.mvcMatchers("/admin/**").hasRole(Role.ADMIN.name())
 				.anyRequest().authenticated()
 		).formLogin(login -> login
@@ -51,9 +50,6 @@ public class SecurityConfig {
 				.logoutRequestMatcher(new AntPathRequestMatcher("/logout")))
 		//ブラウザを閉じて再度開いた場合でも「ログインしたままに」
 		.rememberMe();
-		
-		http.headers().frameOptions().disable();
-		http.csrf().ignoringAntMatchers("/h2-console/**");
 		
 		return http.build();		
 	}
