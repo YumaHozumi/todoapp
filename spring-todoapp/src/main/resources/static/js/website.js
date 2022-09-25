36582ddf79ed837e68fff4e89506fc4f4a4dc02e
YumaHozumi <cpucmptrpython72@gmail.com>
2022-09-25 22:49:17 +0900

    パスワードの表示/非表示機能を追加

---
+++ spring-todoapp/src/main/resources/static/js/website.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const passwordElement = document.getElementById('password');
+const eye = document.getElementById('eye');
+eye.addEventListener('click', function(){
+	if(eye.classList.contains('bi-eye-fill')){
+		eye.classList.remove('bi-eye-fill');
+		eye.classList.add('bi-eye-slash-fill');
+		passwordElement.type = "text";
+	}else if(eye.classList.contains('bi-eye-slash-fill')){
+		eye.classList.remove('bi-eye-slash-fill');
+		eye.classList.add('bi-eye-fill');
+		passwordElement.type = "password";
+	}
+});
