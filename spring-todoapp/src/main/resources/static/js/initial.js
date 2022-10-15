25d51c35c7bdb4a932e48d1e061e94774e79770f
YumaHozumi <cpucmptrpython72@gmail.com>
2022-10-15 21:10:49 +0900

    タスク一覧で日付を見れるように変更

--- spring-todoapp/src/main/resources/static/js/initial.js
+++ spring-todoapp/src/main/resources/static/js/initial.js
@@ -1,6 +1,2 @@
 'use strict'
 
-let popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
-let popoverList = popoverTriggerList.map(function (popoverTriggerElem){
-	return new bootstrap.Popover(popoverTriggerElem);
-});
